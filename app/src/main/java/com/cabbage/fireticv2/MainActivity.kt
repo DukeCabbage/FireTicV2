95b05eb48b0d5b3b71d3c9f6875a24ec75a28f00
DukeCabbage <liaowng@gmail.com>
2018-01-03 00:59:10 -0800

    Main menu ui

--- app/src/main/java/com/cabbage/fireticv2/MainActivity.kt
+++ app/src/main/java/com/cabbage/fireticv2/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.cabbage.fireticv2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import butterknife.ButterKnife
+import butterknife.OnClick
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    @OnClick(R.id.btn_menu_about)
+    fun onClick() {
+        val packageInfo = packageManager.getPackageInfo(this.packageName, 0)
+        val strVersion = "${packageInfo.versionName} (${packageInfo.versionCode})"
+        Snackbar.make(rootView, strVersion, Snackbar.LENGTH_LONG).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+        setSupportActionBar(toolbar)
     }
 }
---
+++ app/src/main/java/com/cabbage/fireticv2/ui/DashboardButton.kt
@@ -0,0 +1,51 @@
+package com.cabbage.fireticv2.ui
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
+import android.support.v4.graphics.drawable.DrawableCompat
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
+import android.view.View
+import butterknife.ButterKnife
+import com.cabbage.fireticv2.R
+import kotlinx.android.synthetic.main.dashboard_button.view.*
+
+class DashboardButton(context: Context, attributeSet: AttributeSet)
+    : CardView(context, attributeSet) {
+
+    private var image: Drawable
+    private var label: String
+    private var contentTint: Int = 0
+
+    init {
+        val typedArray: TypedArray = context.theme
+                .obtainStyledAttributes(attributeSet,
+                                        R.styleable.DashboardButton,
+                                        0, 0)
+
+        try {
+            image = typedArray.getDrawable(R.styleable.DashboardButton_image)
+            label = typedArray.getString(R.styleable.DashboardButton_label)
+            contentTint = typedArray.getColor(R.styleable.DashboardButton_contentTint,
+                                              resources.getColor(R.color.colorTextPrimary))
+            View.inflate(context, R.layout.dashboard_button, this)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        ButterKnife.bind(this, this)
+
+        DrawableCompat.setTint(image, contentTint)
+        ivImage.setImageDrawable(image)
+
+        tvLabel.text = label
+        tvLabel.setTextColor(contentTint)
+
+        this.isClickable = true
+        this.isFocusable = true
+    }
+}
